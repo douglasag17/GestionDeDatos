2f3cfbf9d138ecb203f970bbba5480260438bf99
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-05-17 12:33:46 -0500

    campo fecha

--- Talend/esquemaQuiz.sql
+++ Talend/esquemaQuiz.sql
@@ -3,14 +3,15 @@ use sakila;
 DROP TABLE IF EXISTS sakila.dimTimeQuiz;
 CREATE TABLE sakila.dimTimeQuiz (
 	time_id int PRIMARY KEY AUTO_INCREMENT,
+	fecha DATETIME,
 	año int,
-    mes int,
+	mes int,
 	dia int
 );
 
 DROP TABLE IF EXISTS sakila.factPayment;
 CREATE TABLE sakila.factPayment (
 	time_id int,
-    pagos_dia int,
+	pagos_dia int,
 	foreign key(time_id) references dimTimeQuiz(time_id)
 );
